a2d386c014a17f78be30d61c74b00f04d41f7c7c
Yu Okuda <yu.kyoto2720@gmail.com>
2018-10-20 13:39:38 +0900

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new"
   post "/login" => "sessions#create"
   delete "/logout" => "sessions#destroy"
-  
+
   resources :users
+  resources :account_activations, only: [:edit]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
